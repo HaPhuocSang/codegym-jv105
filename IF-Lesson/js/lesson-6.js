e0dc3cea6a7ebf67773fec7f290d1fbba56c38a5
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-04-22 20:19:03 +0700

    lesson-6-1

--- IF-Lesson/js/lesson-6.js
+++ IF-Lesson/js/lesson-6.js
@@ -1,21 +1,21 @@
 let selectedCommissionPercentage = document.getElementById("btnCommissionPercentage");
 selectedCommissionPercentage.addEventListener("click", showCommissionPercentage);
-function showCommissionPercentage(){
+
+function showCommissionPercentage() {
     let result;
     let commissionPercentage = parseFloat(document.getElementById("commission-percentage").value);
     let totalRevenue = parseFloat(document.getElementById("total-revenue").value);
-    if (!isNaN(commissionPercentage) && !isNaN(totalRevenue)) {
-        if (commissionPercentage >=0) {
-            let money;
-            money = totalRevenue * commissionPercentage / 100;
-            result = "The percentage of commission received is " + money;
-        }
-        else {
-            result = "The commission percentage must not be less than 0."
-        }
-    }
-    else {
+    if (isNaN(commissionPercentage) || isNaN(totalRevenue)) {
         result = "Input total revenue and commission percentage."
+        document.getElementById("result").innerHTML = result;
+        return;
+    }
+    if (commissionPercentage >= 0) {
+        let money;
+        money = totalRevenue * commissionPercentage / 100;
+        result = "The percentage of commission received is " + money;
+    } else {
+        result = "The commission percentage must not be less than 0."
     }
     document.getElementById("result").innerHTML = result;
 }
