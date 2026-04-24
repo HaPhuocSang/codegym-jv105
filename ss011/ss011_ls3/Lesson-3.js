088efe110f8cb367024d39b63e0c63b314e7c421
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-04-24 10:48:05 +0700

    ss011-ls3-1

--- ss011/ss011_ls3/Lesson-3.js
+++ ss011/ss011_ls3/Lesson-3.js
@@ -1,8 +1,8 @@
-function calculateArea(){
+function calculateArea() {
     let lengthSide = parseFloat(document.getElementById("side").value);
     let result;
     let areSquares;
-    if (isNaN(lengthSide) || lengthSide <= 0){
+    if (isNaN(lengthSide) || lengthSide <= 0) {
         document.getElementById("result").innerHTML = "Enter side length / Side length greater than 0";
         return;
     }
