4b850412eceeab9f2043067b958a24bd68dfef31
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-04-24 19:57:59 +0700

    ss011-ls6-1

--- ss011/ss011_ls6/lesson-6.js
+++ ss011/ss011_ls6/lesson-6.js
@@ -1,22 +1,16 @@
 function solveLinearEquations() {
-    let solutionEquation;
-    let result;
     let aNumber = parseFloat(document.getElementById("aNumber").value);
     let bNumber = parseFloat(document.getElementById("bNumber").value);
     if (isNaN(aNumber) || isNaN(bNumber)) {
         document.getElementById("result").innerHTML = "Input a,b.";
         return;
     }
+    let result;
     if (aNumber === 0){
-        if (bNumber === 0){
-            result = "This equation has infinitely many solutions.";
-        }
-        else {
-            result = "This equation has no solution.";
-        }
+        result = bNumber === 0 ? "This equation has infinitely many solutions." : "This equation has no solution.";
     }
     else {
-        solutionEquation = -bNumber / aNumber;
+        let solutionEquation = -bNumber / aNumber;
         result = `x = ${solutionEquation}`;
     }
     document.getElementById("result").innerHTML = result;
