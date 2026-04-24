90ed13065fca75b3090a8776632f30e7b91f8a53
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-04-24 19:44:08 +0700

    ss011-ls3-2

--- ss011/ss011_ls3/lesson-3.js
+++ ss011/ss011_ls3/lesson-3.js
@@ -1,12 +1,9 @@
 function calculateArea() {
     let lengthSide = parseFloat(document.getElementById("side").value);
-    let result;
-    let areSquares;
     if (isNaN(lengthSide) || lengthSide <= 0) {
         document.getElementById("result").innerHTML = "Enter side length / Side length greater than 0";
         return;
     }
-    areSquares = lengthSide * lengthSide;
-    result = `The area of the square is ${areSquares}`;
-    document.getElementById("result").innerHTML = result;
+    let areSquares = lengthSide * lengthSide;
+    document.getElementById("result").innerHTML = `The area of the square is ${areSquares}`;
 }
