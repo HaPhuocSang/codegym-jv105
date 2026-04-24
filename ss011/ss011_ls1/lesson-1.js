e0920b29d89f1f8f339d2e85b566e5bd9a449c80
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-04-24 19:25:18 +0700

    ss011-ls1-1

--- ss011/ss011_ls1/lesson-1.js
+++ ss011/ss011_ls1/lesson-1.js
@@ -1,12 +1,10 @@
 function convertCelsius() {
-    let celsius = parseFloat(document.getElementById("Celsius").value);
-    let fahrenheit;
-    let result;
+    let celsius = parseFloat(document.getElementById("Celsius").value);  
     if (isNaN(celsius)) {
         document.getElementById("result").innerHTML = "Enter the degree Celsius (°C).";
         return;
     }
-    fahrenheit = celsius * 9 / 5 + 32;
-    result = `${celsius}°C is equal to ${fahrenheit}°F`;
+    let fahrenheit = celsius * 9 / 5 + 32;
+    let result = `${celsius}°C is equal to ${fahrenheit}°F`;
     document.getElementById("result").innerHTML = result;
 }
