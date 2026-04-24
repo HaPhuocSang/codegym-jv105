ec094b415ef6fd0d303de150fda14a6c57231a96
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-04-24 19:50:42 +0700

    ss011-ls5-1

--- ss011/ss011_ls5/lesson-5.js
+++ ss011/ss011_ls5/lesson-5.js
@@ -1,6 +1,4 @@
 function calculateAreaOfaRightTriangle() {
-    let result;
-    let areaOfaRightTriangle;
     let baseLength = parseFloat(document.getElementById("baseLength").value);
     let sideHeight = parseFloat(document.getElementById("sideHeight").value);
     if (isNaN(baseLength) || isNaN(sideHeight)) {
@@ -11,7 +9,6 @@ function calculateAreaOfaRightTriangle() {
         document.getElementById("result").innerHTML = "The height and the length of the base cannot be less than zero.";
         return;
     }
-    areaOfaRightTriangle = (baseLength * sideHeight) / 2;
-    result = `The area of the right triangle is ${areaOfaRightTriangle}.`;
-    document.getElementById("result").innerHTML = result;
+    let areaOfaRightTriangle = (baseLength * sideHeight) / 2;
+    document.getElementById("result").innerHTML = `The area of the right triangle is ${areaOfaRightTriangle}.`;
 }
