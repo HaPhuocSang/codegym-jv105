dd0685ca299d8b06228e28a45e3801c355fd38a7
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-04-25 15:40:21 +0700

    ss011-ls12-1

--- ss011/ss011_ls12/lesson-12.js
+++ ss011/ss011_ls12/lesson-12.js
@@ -1,9 +1,7 @@
 function calculateBankInterest() {
-    let A;
     let P = parseFloat(document.getElementById("initialAmount").value);
     let n = parseFloat(document.getElementById("numberMonths").value);
     let r = parseFloat(document.getElementById("monthlyInterest").value) / 100;
-    let result;
     if (isNaN(P) || isNaN(n) || isNaN(r)) {
         document.getElementById("result").innerHTML = "Please enter a number";
         return;
@@ -12,7 +10,6 @@ function calculateBankInterest() {
         document.getElementById("result").innerHTML = "That number must not be less than zero.";
         return;
     }
-    A = P * Math.pow(1 + r, n);
-    result = `The total amount to be paid is ${A}.`;
-    document.getElementById("result").innerHTML = result;
+    let A = P * Math.pow(1 + r, n);
+    document.getElementById("result").innerHTML = `The total amount to be paid is ${A}.`;
 }
