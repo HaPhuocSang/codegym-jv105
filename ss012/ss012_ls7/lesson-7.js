e4ba7d974983749950bc3b3e8728acedd1c7d3d1
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-04-27 11:14:09 +0700

    ss012-ls7

--- ss012/ss012_ls7/lesson-7.js
+++ ss012/ss012_ls7/lesson-7.js
@@ -1,13 +1,18 @@
-let displayNumberSequence = "";
-for (let i = 1; i <= 100; i++) {
-    if (i % 5 === 0 && i % 3 === 0) {
-        displayNumberSequence += "FizzBuzz" + " ";
-    } else if (i % 3 === 0) {
-        displayNumberSequence += "Fizz" + " ";
-    } else if (i % 5 === 0) {
-        displayNumberSequence += "Buzz" + " ";
-    } else {
-        displayNumberSequence += i + " ";
+let selectedNumbersFromOneToHundreds = document.getElementById("btnNumbersFromOneToHundreds");
+selectedNumbersFromOneToHundreds.addEventListener("click", displayNumbersFromOneToHundreds);
+
+function displayNumbersFromOneToHundreds() {
+    let displayNumberSequence = "";
+    for (let i = 1; i <= 100; i++) {
+        if (i % 5 === 0 && i % 3 === 0) {
+            displayNumberSequence += "FizzBuzz" + " ";
+        } else if (i % 3 === 0) {
+            displayNumberSequence += "Fizz" + " ";
+        } else if (i % 5 === 0) {
+            displayNumberSequence += "Buzz" + " ";
+        } else {
+            displayNumberSequence += i + " ";
+        }
     }
+    document.getElementById("result").innerHTML = displayNumberSequence;
 }
-document.getElementById("result").innerHTML = displayNumberSequence;
