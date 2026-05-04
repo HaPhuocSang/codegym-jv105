6ddda9055b6e4299222f788455faab2499208b36
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-04 19:25:02 +0700

    ss012-ls6-1

--- ss012/ss012_ls6/lesson-6.js
+++ ss012/ss012_ls6/lesson-6.js
@@ -1,16 +1,17 @@
 let selectedSumNaturalNumber = document.getElementById("btnSumNaturalNumbers");
 selectedSumNaturalNumber.addEventListener("click", displaySumNaturalNumbers);
+const LAST_NUMBER = 30;
 
 function displaySumNaturalNumbers() {
     let sumDivisibleBySeven = 0;
     let count = 0;
     let i = 0;
-    while (count < 30) {
+    while (count < LAST_NUMBER) {
         if (i % 7 === 0 && i !== 0) {
             sumDivisibleBySeven += i;
             count++;
         }
         i++;
     }
-    document.getElementById("result").innerHTML = sumDivisibleBySeven.toString();
+    document.getElementById("result").textContent = sumDivisibleBySeven.toString();
 }
