680828c8ae626341170c4f736c96d38ca736c5f8
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-04 19:08:38 +0700

    ss012-ls2-1

--- ss012/ss012_ls2/lesson-2.js
+++ ss012/ss012_ls2/lesson-2.js
@@ -1,13 +1,15 @@
+const MIN_TEMPERATURE = 20;
+const MAX_TEMPERATURE = 100;
 let currentTemperature;
 do {
     currentTemperature = parseFloat(prompt("Nhập nhiệt độ hiện tại: "));
     if (isNaN(currentTemperature)) {
         alert("Vui lòng nhập số hợp lệ!");
     }
-    if (currentTemperature < 20) {
+    if (currentTemperature < MIN_TEMPERATURE) {
         alert("Yêu cầu người dùng tăng nhiệt độ.");
     }
-    if (currentTemperature > 100) {
+    if (currentTemperature > MAX_TEMPERATURE) {
         alert("yêu cầu người dùng giảm nhiệt độ.");
     }
 } while (currentTemperature < 20 || currentTemperature > 100 || isNaN(currentTemperature));
