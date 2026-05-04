d034bf16592c90b77c44a33af57d9f6c11f679c1
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-04 19:19:59 +0700

    ss012-ls5-1

--- ss012/ss012_ls5/lesson-5.js
+++ ss012/ss012_ls5/lesson-5.js
@@ -1,15 +1,16 @@
 let selectedBtnSumFibonacci = document.getElementById("btnSumFibonacci");
-selectedBtnSumFibonacci.addEventListener("click", displaySumFibonacci);
+selectedBtnSumFibonacci.addEventListener("click", displaySumFibonacci)
+const LAST_NUMBER = 20;
 
 function displaySumFibonacci() {
     let currentFibonacci = 0;
     let nextFibonacci = 1;
     let displaySum = 0;
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < LAST_NUMBER; i++) {
         displaySum += currentFibonacci;
         let sumFibonacci = currentFibonacci + nextFibonacci;
         currentFibonacci = nextFibonacci;
         nextFibonacci = sumFibonacci;
     }
-    document.getElementById("result").innerHTML = displaySum.toString();
+    document.getElementById("result").textContent = displaySum.toString();
 }
