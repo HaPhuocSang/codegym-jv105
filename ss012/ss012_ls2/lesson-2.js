37c793138ef7366f0ff84fafd5789e1cac09b817
HaPhuocSang <136155087+HaPhuocSang@users.noreply.github.com>
2026-05-06 18:16:12 +0700

    ss012-ls2

---
+++ ss012/ss012_ls2/lesson-2.js
@@ -0,0 +1,15 @@
+let currentTemperature;
+const MIN_TEMPERATURE = 20;
+const MAX_TEMPERATURE = 100;
+do {
+    currentTemperature = parseFloat(prompt("Nhập nhiệt độ hiện tại: "));
+    if (isNaN(currentTemperature)) {
+        alert("Vui lòng nhập số hợp lệ!");
+    }
+    if (currentTemperature < MIN_TEMPERATURE) {
+        alert("Yêu cầu người dùng tăng nhiệt độ.");
+    }
+    if (currentTemperature > MAX_TEMPERATURE) {
+        alert("yêu cầu người dùng giảm nhiệt độ.");
+    }
+} while (currentTemperature < 20 || currentTemperature > 100 || isNaN(currentTemperature));
