e32f9d078ca21eed5873727d221780facc96bb88
hienle-2026 <hien.le@codegym.vn>
2026-05-06 19:12:28 +0700

    ss12: Multiplication table (#48)

---
+++ ss012/multiplication-table/multiplication-table.js
@@ -0,0 +1,16 @@
+let container = document.querySelector('.multiplication-table');
+let table = document.createElement('table')
+table.style.border = "1px solid black";
+for (let i = 1; i < 10; i++) {
+    let tr = document.createElement('tr');
+    for (let j = 2; j < 10; j++) {
+        let td = document.createElement('td');
+        td.style.border = "1px solid black";
+        td.style.padding = "5px";
+        td.style.width = "100px";
+        td.textContent = `${j}x${i}= ${j * i}`;
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
+container.appendChild(table);
