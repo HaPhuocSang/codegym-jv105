9df79b7dbf760c914061fc7b435928585acb74d2
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-08 10:49:29 +0700

    ss014-ls1

---
+++ ss014/ss014-ls1/lesson-1.js
@@ -0,0 +1,41 @@
+const NUMBER_INTEGER = [];
+const MAX_ELEMENTS = 10;
+const LIMIT_NUMBER = 10;
+let selectedBtnInitializeAnArray = document.getElementById("btnInitializeAnArray");
+let selectedBtnDisplayIntegers = document.getElementById("btnDisplayIntegers");
+selectedBtnInitializeAnArray.addEventListener("click", pushInitializeAnArray);
+selectedBtnDisplayIntegers.addEventListener("click", showArrayInteger);
+
+function pushInitializeAnArray() {
+    for (let i = 0; i < MAX_ELEMENTS; i++) {
+        do {
+            NUMBER_INTEGER[i] = parseFloat(prompt(`Nhập số nguyên thứ ${i + 1}:`));
+            if (!Number.isInteger(NUMBER_INTEGER[i])) {
+                alert("Số bạn nhập vào không phải số nguyên!")
+            }
+        } while (!Number.isInteger(NUMBER_INTEGER[i]));
+    }
+    let result = NUMBER_INTEGER.join(", ");
+    alert(`Các giá trị mà bạn vửa nhập vào là: ${result}`)
+}
+
+function showArrayInteger() {
+    if (NUMBER_INTEGER.length <= 0) {
+        alert("Bạn chưa khởi tạo mảng!");
+        return;
+    }
+    let countNumber = 0;
+    let result = [];
+    let txt = "";
+    for (let i = 0; i < NUMBER_INTEGER.length; i++) {
+        if (NUMBER_INTEGER[i] >= LIMIT_NUMBER) {
+            result.push(NUMBER_INTEGER[i]);
+            countNumber++;
+        }
+    }
+    txt = `Có ${countNumber} lớn hơn hoặc bằng ${LIMIT_NUMBER} đó là ${result.join(", ")}`;
+    if (countNumber === 0) {
+        txt = `Không có số nào lớn hơn hoặc bằng ${LIMIT_NUMBER}`;
+    }
+    document.getElementById("result").textContent = txt;
+}
