c0f72f23c5a5b5d221685212d9a1f1782d4e7fd3
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-08 15:35:50 +0700

    ss014-ls6

---
+++ ss014/ss014-ls6/lesson-6.js
@@ -0,0 +1,32 @@
+const NUMBER_INTEGER = [];
+const MIN_ELEMENTS = 1;
+const MAX_ELEMENTS = 10;
+const MAX_RANDOM = 100;
+const MIN_RANDOM = -100;
+const INCLUSIVE_RANGE = 1;
+
+function pushInitializeAnArray() {
+    NUMBER_INTEGER.length = 0;
+    for (let i = 0; i < MAX_ELEMENTS; i++) {
+        NUMBER_INTEGER.push(Math.floor(Math.random() * (MAX_RANDOM - MIN_RANDOM + INCLUSIVE_RANGE)) + MIN_RANDOM);
+    }
+    let result = NUMBER_INTEGER.join(", ");
+    document.getElementById("resultFirst").textContent = `Các giá trị mà bạn vừa khởi tạo là: ${result}`;
+}
+
+function elementInspection() {
+    if (NUMBER_INTEGER.length < MIN_ELEMENTS) {
+        alert("Bạn Chưa khởi tạo mảng!");
+        return;
+    }
+    let inspection = parseFloat(document.getElementById("elementValue").value);
+    if (isNaN(inspection)) {
+        alert("Giá trị nhập vào không hợp lệ!");
+        return;
+    }
+    let result;
+    result = NUMBER_INTEGER.includes(inspection)
+        ? `${inspection} is in the array.`
+        : `${inspection} is not in the array.`;
+    document.getElementById("resultLast").textContent = result;
+}
