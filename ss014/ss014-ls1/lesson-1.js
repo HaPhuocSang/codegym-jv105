a9a8e102f49537a0ba287f9ad262124ccbc7d1a7
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-08 11:41:17 +0700

    ss014-ls1

--- ss014/ss014-ls1/lesson-1.js
+++ ss014/ss014-ls1/lesson-1.js
@@ -1,5 +1,6 @@
 const NUMBER_INTEGER = [];
 const MAX_ELEMENTS = 10;
+const MIN_ELEMENTS = 1;
 const LIMIT_NUMBER = 10;
 let selectedBtnInitializeAnArray = document.getElementById("btnInitializeAnArray");
 let selectedBtnDisplayIntegers = document.getElementById("btnDisplayIntegers");
@@ -16,26 +17,25 @@ function pushInitializeAnArray() {
         } while (!Number.isInteger(NUMBER_INTEGER[i]));
     }
     let result = NUMBER_INTEGER.join(", ");
-    alert(`Các giá trị mà bạn vửa nhập vào là: ${result}`)
+    document.getElementById("resultFirst").textContent = `Các giá trị mà bạn vừa nhập vào là: ${result}`;
 }
 
 function showArrayInteger() {
-    if (NUMBER_INTEGER.length <= 0) {
+    if (NUMBER_INTEGER.length < MIN_ELEMENTS) {
         alert("Bạn chưa khởi tạo mảng!");
         return;
     }
     let countNumber = 0;
     let result = [];
-    let txt = "";
     for (let i = 0; i < NUMBER_INTEGER.length; i++) {
         if (NUMBER_INTEGER[i] >= LIMIT_NUMBER) {
             result.push(NUMBER_INTEGER[i]);
             countNumber++;
         }
     }
-    txt = `Có ${countNumber} lớn hơn hoặc bằng ${LIMIT_NUMBER} đó là ${result.join(", ")}`;
+    let txt = `Có ${countNumber} giá trị lớn hơn hoặc bằng ${LIMIT_NUMBER} đó là ${result.join(", ")}`;
     if (countNumber === 0) {
         txt = `Không có số nào lớn hơn hoặc bằng ${LIMIT_NUMBER}`;
     }
-    document.getElementById("result").textContent = txt;
+    document.getElementById("resultLast").textContent = txt;
 }
