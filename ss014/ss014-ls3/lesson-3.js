f19b48dc1425c4d24a59a4a07d256e7a9bf172b8
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-08 12:25:49 +0700

    ss014-ls3

---
+++ ss014/ss014-ls3/lesson-3.js
@@ -0,0 +1,45 @@
+const NUMBER_INTEGER = [];
+const MIN_ELEMENTS = 1;
+
+function pushInitializeAnArray() {
+    NUMBER_INTEGER.length = 0;
+    let maxElements;
+    do {
+        maxElements = parseFloat(prompt("Nhập số lượng phần tử muốn khởi tạo:"));
+        if (!Number.isInteger(maxElements) || maxElements <= 0) {
+            alert("Giá trị nhập vào không hợp lệ!");
+        }
+    } while (!Number.isInteger(maxElements) || maxElements <= 0);
+    for (let i = 0; i < maxElements; i++) {
+        NUMBER_INTEGER.push(Math.floor(Math.random() * 1000));
+    }
+    let result = NUMBER_INTEGER.join(", ");
+    document.getElementById("resultFirst").textContent = `Các giá trị mà bạn vừa nhập vào là: ${result}`;
+}
+
+function showLargestElement() {
+    if (NUMBER_INTEGER.length < MIN_ELEMENTS) {
+        alert("Bạn Chưa khởi tạo mảng!");
+        return;
+    }
+    let max = NUMBER_INTEGER[0];
+    for (let i = 0; i < NUMBER_INTEGER.length; i++) {
+        if (max <= NUMBER_INTEGER[i]) {
+            max = NUMBER_INTEGER[i];
+        }
+    }
+    document.getElementById("resultSecond").textContent = `Phần tử lớn nhất có giá trị là ${max}!`;
+}
+
+function showAverageElement() {
+    if (NUMBER_INTEGER.length < MIN_ELEMENTS) {
+        alert("Bạn Chưa khởi tạo mảng!");
+        return;
+    }
+    let sum = 0;
+    for (let i = 0; i < NUMBER_INTEGER.length; i++) {
+        sum += NUMBER_INTEGER[i];
+    }
+    let aver = sum / NUMBER_INTEGER.length;
+    document.getElementById("resultThird").textContent = `Giá trị trung bình của các phần tử trong mảng là ${aver}!`;
+}
