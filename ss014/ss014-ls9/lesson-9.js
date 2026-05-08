35e076d57c4fcb0f4002b70d95078daaa735f1b1
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-08 17:10:16 +0700

    ss014-ls9

---
+++ ss014/ss014-ls9/lesson-9.js
@@ -0,0 +1,39 @@
+const A=[];
+const B = [];
+const C = [];
+const MIN_ELEMENTS = 1;
+const MAX_ELEMENTS = 10;
+const MAX_RANDOM = 100;
+const MIN_RANDOM = -100;
+const INCLUSIVE_RANGE = 1;
+
+function pushInitializeAnArray() {
+    A.length = 0;
+    B.length = 0;
+    for (let i = 0; i < MAX_ELEMENTS; i++) {
+        A.push(Math.floor(Math.random() * (MAX_RANDOM - MIN_RANDOM + INCLUSIVE_RANGE)) + MIN_RANDOM);
+    }
+    let resultArrayA = A.join(", ");
+    for (let i = 0; i < MAX_ELEMENTS; i++) {
+        B.push(Math.floor(Math.random() * (MAX_RANDOM - MIN_RANDOM + INCLUSIVE_RANGE)) + MIN_RANDOM);
+    }
+    let resultArrayB = B.join(", ");
+    document.getElementById("resultFirst").textContent = `Các giá trị mảng A mà bạn vừa khởi tạo là: ${resultArrayA}`;
+    document.getElementById("resultSecond").textContent = `Các giá trị mảng B mà bạn vừa khởi tạo là: ${resultArrayB}`;
+}
+
+function joinArraysAAndB() {
+    C.length = 0;
+    if (A.length < MIN_ELEMENTS || B.length < MIN_ELEMENTS) {
+        alert("Bạn Chưa khởi tạo mảng!");
+        return;
+    }
+    for (let i = 0; i < A.length; i++) {
+        C.push(A[i]);
+    }
+    for (let i = 0; i < B.length; i++) {
+        C.push(B[i]);
+    }
+    let resultArrayC = C.join(", ");
+    document.getElementById("resultThird").textContent = `Các giá trị mảng C sau khi nối mảng A và B là: ${resultArrayC}`;
+}
