78a100f7eb263233f161663a97f40e3cc4388764
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-08 19:02:44 +0700

    ss014-dictionary

---
+++ ss014/ss014-dictionary/dictionary.js
@@ -0,0 +1,19 @@
+const ENGLISH_WORDS = ["hello","book","computer","apple","school"];
+const VIETNAMESE_WORDS = ["xin chào","quyển sách","máy tính","quả táo","trường học"];
+
+function showVietnameseWord() {
+    let englishWord = document.getElementById("englishWord").value;
+    if (englishWord.length === 0) {
+        alert("Bạn chưa nhập từ tiếng anh!");
+        return;
+    }
+    englishWord = englishWord.toLowerCase();
+    let result;
+    if (ENGLISH_WORDS.indexOf(englishWord) !== -1) {
+        result = VIETNAMESE_WORDS[ENGLISH_WORDS.indexOf(englishWord)];
+    } else {
+        result = "Không tìm thấy!";
+    }
+    document.getElementById("result").textContent = result;
+
+}
