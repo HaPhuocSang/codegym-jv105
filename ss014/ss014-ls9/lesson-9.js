dc8e63b199a6b1a086425a1f9174f95f0eec2e42
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-13 20:54:59 +0700

    ss014-ls9

--- ss014/ss014-ls9/lesson-9.js
+++ ss014/ss014-ls9/lesson-9.js
@@ -1,21 +1,24 @@
 const A = [];
 const B = [];
-const C = [];
+let C = [];
 const MAX_ELEMENTS = 10;
 const MAX_RANDOM = 100;
 const MIN_RANDOM = -100;
 const INCLUSIVE_RANGE = 1;
 
+function addRandomlyToTheArray(array) {
+    for (let i = 0; i < MAX_ELEMENTS; i++) {
+        array.push(Math.floor(Math.random() * (MAX_RANDOM - MIN_RANDOM + INCLUSIVE_RANGE)) + MIN_RANDOM);
+    }
+    return array;
+}
+
 function pushInitializeAnArray() {
     A.length = 0;
     B.length = 0;
-    for (let i = 0; i < MAX_ELEMENTS; i++) {
-        A.push(Math.floor(Math.random() * (MAX_RANDOM - MIN_RANDOM + INCLUSIVE_RANGE)) + MIN_RANDOM);
-    }
+    addRandomlyToTheArray(A);
     let resultArrayA = A.join(", ");
-    for (let i = 0; i < MAX_ELEMENTS; i++) {
-        B.push(Math.floor(Math.random() * (MAX_RANDOM - MIN_RANDOM + INCLUSIVE_RANGE)) + MIN_RANDOM);
-    }
+    addRandomlyToTheArray(B)
     let resultArrayB = B.join(", ");
     document.getElementById("resultFirst").textContent = `Các giá trị mảng A mà bạn vừa khởi tạo là: ${resultArrayA}`;
     document.getElementById("resultSecond").textContent = `Các giá trị mảng B mà bạn vừa khởi tạo là: ${resultArrayB}`;
@@ -23,16 +26,7 @@ function pushInitializeAnArray() {
 
 function joinArraysAAndB() {
     C.length = 0;
-    if (!A.length || !B.length) {
-        alert("Bạn Chưa khởi tạo mảng!");
-        return;
-    }
-    for (let i = 0; i < A.length; i++) {
-        C.push(A[i]);
-    }
-    for (let i = 0; i < B.length; i++) {
-        C.push(B[i]);
-    }
+    C = A.concat(B);
     let resultArrayC = C.join(", ");
     document.getElementById("resultThird").textContent = `Các giá trị mảng C sau khi nối mảng A và B là: ${resultArrayC}`;
 }
