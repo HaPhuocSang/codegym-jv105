e80aa7b88b749573c90f11b9129557cf861d523a
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-17 18:41:36 +0700

    ss018-temperature

---
+++ ss018/ss018-temperature/temperature.js
@@ -0,0 +1,35 @@
+function Temperature(celsius) {
+    this.celsius = celsius;
+    this.minCelsius();
+}
+
+Temperature.prototype.minCelsius = function () {
+    if (this.celsius < -273) {
+        alert("Nhiệt độ nhỏ nhất là -273°C!");
+        throw new Error("Invalid temperature");
+    }
+}
+
+
+Temperature.prototype.switchToFahrenheit = function () {
+    return this.celsius * 1.8 + 32;
+}
+Temperature.prototype.switchToKelvin = function () {
+    return this.celsius + 273.15;
+}
+
+function showResult() {
+    let celsius = Number(document.getElementById("celsiusValue").value);
+    if (isNaN(celsius)) {
+        alert("Giá trị đầu vào không hợp lệ!");
+        return;
+    }
+    try {
+        let temperature = new Temperature(celsius);
+        document.getElementById("resultFirst").textContent = `Chuyển °C sang °F là ${temperature.switchToFahrenheit()}°F.`;
+        document.getElementById("resultSecond").textContent = `Chuyển °C sang °K là ${temperature.switchToKelvin()}°K.`;
+    } catch (error) {
+        document.getElementById("resultFirst").textContent = "";
+        document.getElementById("resultSecond").textContent = "";
+    }
+}
