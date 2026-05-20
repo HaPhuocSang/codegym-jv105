8d9376c26120e098b298a43a541bf38a46cbc184
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-20 19:44:25 +0700

    squares

---
+++ practice/squares/squares.js
@@ -0,0 +1,9 @@
+function squareVal(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = Math.pow(arr[i], 2);
+    }
+    return arr;
+}
+
+let arr = [1,5,10, -2];
+alert(`Thay thế mỗi phần tử trong mảng bằng một phần tử có giá trị bằng bình phương của giá trị ban đầu: ${squareVal(arr)}`)
