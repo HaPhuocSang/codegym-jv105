41595008c705c3f4ec7e239c1d84eb8cdfc73797
HaPhuocSang <haphuocsang.dn2003@gmail.com>
2026-05-20 19:31:16 +0700

    array-odd

---
+++ practice/array-odd/array-odd.js
@@ -0,0 +1,11 @@
+function oddNumbers() {
+    let arr = [];
+    for (let i = 1; i <= 50; i++) {
+        if ((i % 2)) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+
+alert(`Mảng gồm các phần từ là các số lẻ từ 1 đến 50 là: ${oddNumbers()}`);
