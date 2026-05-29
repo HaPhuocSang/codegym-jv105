6f4b5d18da6a2b2770e854e7ad9b62a10731ade3
HaPhuocSang <136155087+HaPhuocSang@users.noreply.github.com>
2026-05-29 19:20:21 +0700

    Lesson 2 (#98)

---
+++ casestudy-module-1/lesson-2/lesson-2.js
@@ -0,0 +1,22 @@
+function checkNumber(num) {
+    return !isNaN(num) && num > 0
+}
+function checkTriangle(a,b,c) {
+    if (a + b <= c || a + c <= b || c + b <= a) {
+        return "Không phải là tam giác";
+    }
+    let triangle = [a,b,c];
+    triangle.sort((a, b) => b - a);
+    return Math.pow(triangle[0], 2) === Math.pow(triangle[1], 2) + Math.pow(triangle[2], 2) ? "Là tam giác vuông!" : "Là tam giác thường!";
+}
+
+function showResult() {
+    let firstTriangleSide = Number(document.getElementById("firstTriangleSide").value);
+    let secondTriangleSide = Number(document.getElementById("secondTriangleSide").value);
+    let thirdTriangleSide = Number(document.getElementById("thirdTriangleSide").value);
+    if (!checkNumber(firstTriangleSide) || !checkNumber(secondTriangleSide) || !checkNumber(thirdTriangleSide)) {
+        alert("Cạnh tam giác không được là số âm và không được để trống!");
+        return;
+    }
+    document.getElementById("result").textContent = checkTriangle(firstTriangleSide, secondTriangleSide, thirdTriangleSide);
+}
